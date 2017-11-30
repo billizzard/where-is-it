9b157eff0003ff17efe70e75333dad6efd4d6a76
billizzard <billizzard@mail.ru>
2017-12-01 00:16:51 +0300

    add profile

---
+++ migrations/m171129_132039_change_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m171129_132039_change_column extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m171129_132039_change_column cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m171129_132039_change_column cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
